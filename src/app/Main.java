994b803034a50b43097509ddfd9c848511674d95
Barret22 <vviktoroduvan@gmail.com>
2024-12-11 19:47:13 +0100

    Dz4.1.variable reuse

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,35 +3,37 @@ package app;
 public class Main {
     public static void main(String[] args) {
         // продукт 1
-        String product1Name = "smartphone";
+        String productName = "smartphone";
         // цена за единицу в евро
-        double product1Price = 799.56;
+        double productPrice = 799.56;
         //количество единиц проданных в день
-        int product1Quantity = 15;
+        int productQuantity = 15;
         // количество дней
-        int product1Days = 5;
+        int productDays = 5;
 
         // Расчеты продукта 1
-        double product1TotalSales = product1Price * product1Quantity * product1Days;
-        double product1DailySales = product1TotalSales / product1Days;
+        double productTotalSales = productPrice * productQuantity * productDays;
+        double productDailySales = productTotalSales / productDays;
 
-        // продукт 1
-        String product2Name = "laptop";
+        // Итог
+        System.out.printf("Product No 1: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.\n",
+                productName, productDays, productTotalSales, productDailySales);
+
+        // продукт 2
+        productName = "laptop";
         // цена за единицу в евро
-        double product2Price = 1498.12;
+        productPrice = 1498.12;
         //количество единиц проданных в день
-        int product2Quantity = 10;
+        productQuantity = 10;
         // количество дней
-        int product2Days = 7;
+        productDays = 7;
 
         // Расчеты продукта 2
-        double product2TotalSales = product2Price * product2Quantity * product2Days;
-        double product2DailySales = product2TotalSales / product2Days;
+        productTotalSales = productPrice * productQuantity * productDays;
+        productDailySales = productTotalSales / productDays;
 
-        // Итоги
-        System.out.printf("Product No 1: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.\n",
-                product1Name, product1Days, product1TotalSales, product1DailySales);
+        // Итог
         System.out.printf("Product No 2: %s,\ntotal sales for %d days is EUR %.2f,\nsales by day is EUR %.2f.\n",
-                product2Name, product2Days, product2TotalSales, product2DailySales);
+                productName, productDays, productTotalSales, productDailySales);
     }
 }
